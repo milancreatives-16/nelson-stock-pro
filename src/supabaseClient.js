7d5cd5aed348ad4006708088c171eb5a5292ff90
Denis Mwanthi <denismwanthi51@gmail.com>
2026-05-14 04:36:37 +0300

    first Nelson Stock Pro version

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://stkgjqvvpqujnohmzzys.supabase.co";
-const supabaseAnonKey = "sb_publishable_qQKucNi8NJ2O670naqohFQ_IkbhyBQG";
+const supabaseUrl = "https://hcjhmwrslvxuccnisrps.supabase.co";
+const supabaseAnonKey = "sb_publishable_PL4g3W-VWBFb_Qll1zcp7g_MIBG1f7Y";
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
